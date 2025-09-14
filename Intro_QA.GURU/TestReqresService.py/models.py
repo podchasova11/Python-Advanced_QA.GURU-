d62b3f0a2927aa9a8d101555483fb6855adce140
Mila Podchasova <Pod4asova20@gmail.com>
2025-09-15 00:09:49 +0300

    Create models.py

---
+++ Intro_QA.GURU/TestReqresService.py/models.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
+
+class UserData(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    avatar: str
+
+
+class SupportInfo(BaseModel):
+    url: str
+    text: str
+
+
+class SuccessRegisterData(BaseModel):
+    id: int
+    token: str
+
+
+class SuccessLoginData(BaseModel):
+    token: str
+
+
+class CreateUserRequest(BaseModel):
+    name: str
+    job: str
+
+
+class CreatedUserData(BaseModel):
+    name: str
+    job: str
+    id: int
+    createdAt: str
+
+
+class UpdatedUserData(BaseModel):
+    name: str
+    job: str
+    updatedAt: str
+
+
+class ResponseModel(BaseModel):
+    data: UserData
+    support: SupportInfo
+  
