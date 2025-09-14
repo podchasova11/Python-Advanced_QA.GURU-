5908a26e77584698dd1d630ad1673fc23e31b7ee
Mila Podchasova <Pod4asova20@gmail.com>
2025-09-15 00:02:20 +0300

    Create conftest.py

---
+++ Intro_QA.GURU/TestReqresService.py/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+PROD = "https://reqres.in" 
+TEST = "http://127.0.0.1:8000"
+
+USERS_API = "/api/users"  
+REGISTER_API = "/api/register"
+LOGIN_API = "/api/login"
+
+
+@pytest.fixture(params=[PROD, TEST])
+def domain_url(request):
+    return request.param
+
+
+@pytest.fixture()
+def base_endpoint(domain_url):
+    base_endpoint = f"{domain_url}{USERS_API}"
+    register_endpoint = f"{domain_url}{REGISTER_API}"
+    login_endpoint = f"{domain_url}{LOGIN_API}"
+    return base_endpoint, register_endpoint, login_endpoint
