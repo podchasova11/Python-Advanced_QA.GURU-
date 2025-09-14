ec3fdbd45a8747d4d506d6cac8516cae73efe368
Mila Podchasova <Pod4asova20@gmail.com>
2025-09-15 00:11:23 +0300

    Create shemas.py

---
+++ Intro_QA.GURU/TestReqresService.py/shemas.py
@@ -0,0 +1,39 @@
+post_users = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {
+        "id": {
+            "type": "string"
+        },
+        "createdAt": {
+            "type": "string"
+        },
+        "job": {
+            "type": "string"
+        },
+        "name": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "id",
+        "createdAt",
+        "job",
+        "name"
+    ]
+}
+
+post_login_success = {
+
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "token": {
+            "type": "string",
+            "minLength": 1
+        }
+    },
+    "required": ["token"],
+    "additionalProperties": False
+}
